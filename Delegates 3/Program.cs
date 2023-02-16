ef6bd4b642d9a35bc9f4a5ef97ffc09c7c2b8603
gesha <genabro@ukr.net>
2023-02-16 02:16:24 +0200

    commit

---
+++ Delegates 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+namespace delegates3
+{
+    class Program
+    {
+        delegate float Givesr(int x,int y,int z);
+        static void Main()
+        {
+           
+            Givesr givesr = delegate (int x, int y, int z)
+            {
+                float sr = 0;
+                sr += (x + y + z)/3;
+                return sr;
+            };
+            float result = givesr(3,21,6 );
+            Console.WriteLine($"Середнє арифметичне:{result}");
+        }
+    }
+}
