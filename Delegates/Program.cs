338fb70ac2b7220303ba551fc7ab11399c70bb10
gesha <genabro@ukr.net>
2023-02-17 16:02:18 +0200

    Some changes

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -7,35 +7,32 @@ using System.Threading.Tasks;
 namespace delegati
 {
     
-    delegate int Operation(int x, int y);
+    delegate void Operation(int x, int y);
     class Program
     {
         static void Main()
         {
-
-            Console.WriteLine("Додавання: 2 + 9");
-
-            Operation sum = (int x, int y) => x + y;
-            int sumresult = sum(9, 2);
-            Console.WriteLine(sumresult);
-            Console.WriteLine("Віднімання: -10 - (-10)");
-            Operation substract = (int x, int y) => x - y;
-            int substractresult = substract(-10, -10);
-            Console.WriteLine(substractresult);
-            Console.WriteLine("Множення: 4 * 5");
-            Operation multiply = (x, y) => x * y;
-            int multiplyResult = multiply(4, 5);
-            Console.WriteLine(multiplyResult);
-            Console.WriteLine("Ділення: 10 / 0");
-            Operation divide = (x, y) =>
+            Operation sum = (x, y) => Console.WriteLine($"{x} + {y} = {x + y}");
+            sum(9, 2);
+            Operation substract = (x, y) => Console.WriteLine($"{x} - {y} = {x - y}");
+            substract(10, 3);
+            Operation multiply = (x, y) => Console.WriteLine($"{x} * {y} = {x * y}");
+            multiply(11, 4);
+            Operation divide = Divide;
+            divide(10, 0);  
+            divide(9, 3);
+            void Divide(int x, int y)
             {
                 if (y == 0)
-                    throw new DivideByZeroException("На нуль ділити не можна!");
-                else return x / y;
-
-            };
-            int divideresult = divide(10, 0);
-            Console.WriteLine(divideresult);
+                {
+                    Console.WriteLine("На нуль ділити не можна!");
+                }
+                else
+                {
+                    Console.WriteLine($"{x} / {y} = {x / y}");
+                }
+            }
+                
         }
     }
 }
